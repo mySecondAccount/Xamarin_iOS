6581b75d0e1500370eb35e816ca9dcc7e2c372d1
mySecondAccount <mlee1026@LAMU02RH4VDG8WP.uhc.com>
2016-05-26 13:06:54 -0500

    More Examples
    Tuh-duh

---
+++ HelloForms/HelloForms/HelloForms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloForms
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			/*Code I added*/
+			var btn = new Button {
+				Text = "Click me!"
+			};
+
+			int count = 1;
+
+			btn.Clicked += (object sender, EventArgs e) => {
+				btn.Text = string.Format("Clicked {0} times.", count);
+				count++;
+			};
+
+			/*End*/
+
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						/*Code I added*/
+						btn
+						/*End*/
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ TipCalculatorAndroid/TipCalculatorAndroid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace TipCalculatorAndroid
+{
+	[Activity (Label = "TipCalculatorAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		EditText inputBill;
+		Button calculateButton;
+		TextView outputTip;
+		TextView outputTotal;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			inputBill = FindViewById<EditText> (Resource.Id.inputBill);
+			calculateButton = FindViewById<Button> (Resource.Id.calculateButton);
+			outputTip = FindViewById<TextView> (Resource.Id.outputTip);
+			outputTotal = FindViewById<TextView> (Resource.Id.outputTotal);
+
+			calculateButton.Click += OnClick;
+
+		}
+
+		void OnClick(object sender, EventArgs e)
+		{
+			string s = inputBill.Text;
+
+			var bill = double.Parse (s);
+
+			double tip = bill * 0.15;
+			double total = bill + tip;
+
+			outputTip.Text = tip.ToString ();
+			output
+		}
+	}
+}
